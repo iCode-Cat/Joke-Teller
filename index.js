05dabd30b937d3950ac56793a05025f70db00bea
iCode-Cat <devranboyac8@gmail.com>
2020-11-10 20:37:28 +0200

    main

---
+++ index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.button');
+
+//Call a function when click the button. 
+button.onclick = fire;
+
+//It manages all functions.
+function fire() {
+alert('Not Hello');
+}
